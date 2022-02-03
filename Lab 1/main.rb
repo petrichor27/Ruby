8f5b5d298bc3fc6ff08e47828d3ae48a71e2b69b
petrichor27 <elisabetbabenko@gmail.com>
2022-02-03 16:59:54 +0300

    ex 3.1

--- Lab 1/main.rb
+++ Lab 1/main.rb
@@ -3,4 +3,20 @@
 #2
 puts("Enter your name:")
 name=gets.chop
-puts("Hello, #{name}!")
+puts("Привет, #{name}!")
+#3
+puts("Какой язык твой любимый?")
+lang=gets.chop
+
+#var 1
+if lang=="ruby"
+  then puts("Ты подлиза!")
+else
+  if lang=="c#" or lang=="c++"
+  then puts("Как скучно!")
+  else if lang=="python"
+         then puts("Слишком легко!")
+       else puts("Скоро будет ruby")
+       end
+  end
+end
