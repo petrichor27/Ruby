60c84e7c6f7caf85a9407d4971e5e5cc8ee0523d
petrichor27 <elisabetbabenko@gmail.com>
2022-02-03 20:43:31 +0300

    ex 1/4

--- Lab 1/main.rb
+++ Lab 1/main.rb
@@ -1,14 +1,17 @@
-#1
+#1.1
 #puts("Hello World!")
-#2
-puts("Enter your name:")
-name=gets.chop
-puts("Привет, #{name}!")
-#3
-puts("Какой язык твой любимый?")
-lang=gets.chop
+
+#1.2
+#puts("Enter your name:")
+#name=gets.chop
+#puts("Привет, #{name}!")
+
+#1.3
+#puts("Какой язык твой любимый?")
+#lang=gets.chop
 
 #var 1
+=begin
 if lang=="ruby"
   then puts("Ты подлиза!")
 else
@@ -20,8 +23,10 @@ else
        end
   end
 end
+=end
 
 #var 2
+=begin
 answer = if lang=="ruby"
            "Ты подлиза!"
        else
@@ -34,11 +39,23 @@ answer = if lang=="ruby"
          end
        end
 puts answer
+=end
 
 #var 3
+=begin
 case lang
 when "ruby" then puts("Ты подлиза!")
 when "c#", "c++" then puts("Как скучно!")
 when "python" then puts("Слишком легко!")
 else puts("Скоро будет ruby")
 end
+=end
+
+#1.4
+puts "Введите команду ОС"
+com1=gets.chop
+puts(`#{com1}`)
+
+puts "Введите команду Ruby:"
+com2 = gets.chop
+puts eval(com2)
