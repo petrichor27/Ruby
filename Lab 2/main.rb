f82d726a5a5c0ce3533ea6f72db92b571975a656
petrichor27 <elisabetbabenko@gmail.com>
2022-02-15 23:32:24 +0300

    ex 1/4

--- Lab 2/main.rb
+++ Lab 2/main.rb
@@ -68,6 +68,7 @@ def out(*arr)
     arr.each do |el|
       puts "Name: #{el.name}  Phone: #{el.phone}\nDuties:"
       el.printDuties
+      puts ""
     end
 end
 
@@ -75,15 +76,15 @@ end
 dep_j = Department.new("Department of justice", "8(123)1248525","Control of the activities of the notary")
 dep_e = Department.new("Department of energy", "8(123)4523432","implementation of state policy in the field of the fuel and energy complex")
 dep_d = Department.new("Department of defense", "8(123)1111111","command of the armed forces","controls the financial, economic and economic activities of the Armed Forces")
-puts "*** Список объектов ***"
+puts "\n*** Список объектов ***"
 out(dep_j,dep_e,dep_d)
-puts "*** Добавление ***"
+puts "\n*** Добавление ***"
 dep_j.setDuty("Control of court activities")
 out(dep_j)
-puts "Обязанность №1: " + dep_j.getDuty(1)
-puts "*** Обновление ***"
+puts "\nОбязанность №1: " + dep_j.getDuty(1)
+puts "\n*** Обновление ***"
 dep_j.updateDuty(1,"Improving the legal literacy of the population")
 out(dep_j)
-puts "*** Удаление ***"
+puts "\n*** Удаление ***"
 dep_j.deleteDuty(1)
 out(dep_j)
