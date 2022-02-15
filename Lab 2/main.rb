e2e518f5210e1c9737a4671a963fd847bed6a491
petrichor27 <elisabetbabenko@gmail.com>
2022-02-15 22:27:33 +0300

    ex 1/2

--- Lab 2/main.rb
+++ Lab 2/main.rb
@@ -1,7 +1,12 @@
 class Department
   def initialize (name, phone)
-  @name, @phone = name, phone
+  @name = name
+    if /8\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/ =~ phone
+      @phone = phone
+    else raise ArgumentError.new("Это не номер телефона!")
+    end
   end
+
   attr_accessor :name
   attr_accessor :phone
 =begin
@@ -31,3 +36,13 @@ class Department
 =end
 end
 
+
+def out(*arr)
+    arr.each { |el| puts "Name: #{el.name}  Phone: #{el.phone}" }
+end
+
+
+dep_j = Department.new("Department of justice", "8(123)1248525")
+dep_e = Department.new("Department of energy", "8(123)4523432")
+dep_d = Department.new("Department of defense", "8(123)1111111")
+out(dep_j,dep_e,dep_d)
