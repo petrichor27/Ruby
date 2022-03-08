787d101a3cbf1e2ba646312f712d3cbbcd38a42d
petrichor27 <elisabetbabenko@gmail.com>
2022-03-08 20:58:06 +0300

    ex 3/3

--- Lab 2/main.rb
+++ Lab 2/main.rb
@@ -194,44 +194,116 @@ write_to_yaml("yaml_text.yaml", deps)
 class Department_list
   def initialize(*list)
     @dep_list = Array.new
-    #2
+    #3.2
     list.each { |i| add_note(i) }
     @index = 0
   end
-  #2
+
+  #3.2
   def add_note(dep)
     @dep_list.append(dep)
   end
+
   def choose_note(index)
     @index = index
   end
+
   def change_note(new_dep)
     @dep_list[@index] = new_dep
   end
+
   def get_note
     @dep_list[@index]
   end
+
   def delete_note
     @dep_list.delete_at(@index)
   end
+
   def to_s
     s = "\n"
     @dep_list.each_index { |i| s += "#{i})))  "+@dep_list[i].to_s }
     s
   end
+
+  #3.3
+  def Department_list.from_txt(file)
+    reg_name = /^"[\w| |,|.]+/
+    reg_phone = /8\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
+    reg_duties = /\(["[\w| |,|.]+"]*\)$/
+    deps = Department_list.new
+    File.open(file,"r") do |f|
+      while (line = f.gets)
+        name = reg_name.match(line).to_s
+        name[0]=''
+        phone = reg_phone.match(line).to_s
+        duties = reg_duties.match(line).to_s
+        d = find_duties(duties)
+        dep = Department.new(name, phone)
+        (0..d.length-1).each { |x| dep.set_duty(d[x]) }
+        deps.add_note(dep)
+      end
+    end
+    deps
+  end
+
+  def each
+    @dep_list.each  do |dep|
+     yield dep
+    end
+  end
+
+  def Department_list.to_txt(file, deps)
+    File.open(file,"w") do |f|
+      deps.each do |x|
+        f.puts "\"#{x.name}\", #{x.phone}, (#{x.duties_for_txt})"
+      end
+    end
+  end
+
+  def Department_list.from_yaml(file)
+    store = YAML::Store.new file
+    arr_lines = ""
+    File.open(file,"r") do |f|
+      while (line = f.gets)
+        arr_lines+= line
+      end
+    end
+    deps = store.load(arr_lines)
+    d = Department_list.new
+    deps.each { |i| d.add_note(i) }
+    d
+  end
+
+  def Department_list.to_yaml(file, deps)
+    File.open(file,"w") do |f|
+      f.puts YAML.dump(deps)
+    end
+  end
 end
 
 
-
+#3.2
 =begin
 a = Department_list.new(dep_j,dep_s)
 a.add_note(dep_d)
 a.add_note(dep_e)
 puts "Все:", a
+
 a.choose_note(2)
 a.change_note(dep_j)
 puts "Измененная:", a.get_note
 a.choose_note(0)
 a.delete_note
 puts "После удаления:", a
+=end
+
+#3.3
+=begin
+b = Department_list.from_yaml "yaml_text.yaml"
+puts b
+Department_list.to_txt("Department.txt",b)
+a = Department_list.from_txt("Department.txt")
+puts a
+Department_list.to_yaml("yaml_text.yaml",a)
 =end
